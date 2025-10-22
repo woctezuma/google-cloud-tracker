51c5c68d988e56bd36a4481e164e40240cda6ea4
Wok <woctezuma@users.noreply.github.com>
2025-10-22 16:59:01 +0200

    Display info during the run of the main script

--- update.py
+++ update.py
@@ -5,22 +5,28 @@ from src.object_utils import find_new_objects, save_objects
 
 
 def check_for_updates(*, debug: bool = False) -> None:
+    print("Fetching objects")
     objects = fetch_all_pages(sort_by_date=True)
 
+    print("Loading hashes from disk")
     try:
         hashes = load_hashes()
     except FileNotFoundError:
         hashes = set()
 
     if debug:
+        print("Saving objects to disk")
         save_objects(objects)
 
+    print("Saving hashes to disk")
     save_hashes(objects)
 
     if hashes:
+        print("Finding new objects based on previously loaded hashes")
         new_objects, _ = find_new_objects(objects, hashes)
 
         if new_objects:
+            print("Sending messages about new objects to Discord")
             notify_discord(new_objects)
 
 
