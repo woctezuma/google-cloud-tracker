af4c86e1496c73797575bbb3eb7a4b7b14422a76
Wok <woctezuma@users.noreply.github.com>
2025-10-22 16:32:50 +0200

    Find new objects

--- src/object_utils.py
+++ src/object_utils.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-from src.constants import DATA_FOLDER, TIME_FIELD
+from src.constants import DATA_FOLDER, HASH_FIELD, TIME_FIELD
 
 DATA_FNAME: str = f"{DATA_FOLDER}bucket_data.json"
 INDENT_LENGTH: int = 2
@@ -22,3 +22,13 @@ def load_objects(*, sort_by_date: bool = True) -> list:
     if sort_by_date:
         objects = sort_objects(objects)
     return objects
+
+
+def find_new_objects(objects: list, hashes: set[str]) -> (list, set[str]):
+    new_objects = []
+    for obj in objects:
+        if obj[HASH_FIELD] not in hashes:
+            new_objects.append(obj)
+            hashes.add(obj[HASH_FIELD])
+
+    return new_objects, hashes
